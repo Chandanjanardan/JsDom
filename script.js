089bbf27b3c9f928ecfaa9a37c133d6cf563ded7
Chandan Janardan <chandanjanardan@gmail.com>
2023-01-11 23:54:39 +0530

    adding dom

---
+++ script.js
@@ -0,0 +1,24 @@
+// const p=document.querySelector(".messages").innerHTML;
+// console.log(p)
+
+// How to get parent nodes
+
+// const heading=document.querySelector(".heading");
+// const parent =heading.parentNode;
+// console.log(parent)
+
+// childer node
+
+// const parent =document.querySelector(".parent");
+// console.log(parent.childNodes)
+
+// next element sibling
+
+// const heading=document.querySelector(".heading");
+// const sibling=heading.nextElementSibling;
+// console.log(sibling)
+
+// previous element sibling
+const subheading=document.querySelector("h3");
+const previousSib=subheading.previousElementSibling.innerHTML;
+console.log(previousSib)
